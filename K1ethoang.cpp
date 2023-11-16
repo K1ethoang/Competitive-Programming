a73557289f3988f7bc6d8c902c83f4e3bbb3e2b1
Kiet Hoang <kiethoang101@gmail.com>
2023-11-16 09:04:47 +0700

    Update: Template CP

--- K1ethoang.cpp
+++ K1ethoang.cpp
@@ -35,10 +35,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define faster()                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
+#define faster()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
 #define test()    \
     int test;     \
     cin >> test;  \
